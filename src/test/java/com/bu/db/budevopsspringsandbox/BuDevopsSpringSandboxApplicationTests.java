2461f57d3ee03bc480a0efeec536302b73c3b113
Anton Rasshchektaiev <anton.rasshchektaiev@auto1.com>
2023-10-23 12:28:25 +0300

    test is holyday

--- src/test/java/com/bu/db/budevopsspringsandbox/BuDevopsSpringSandboxApplicationTests.java
+++ src/test/java/com/bu/db/budevopsspringsandbox/BuDevopsSpringSandboxApplicationTests.java
@@ -3,11 +3,19 @@ package com.bu.db.budevopsspringsandbox;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.time.LocalDate;
+
 @SpringBootTest
 class BuDevopsSpringSandboxApplicationTests {
 
     @Test
     void contextLoads() {
+    }
+
+    @Test
+    void isHolyday() {
+        LocalDate holyday = LocalDate.parse("2023-12-10");
+        assert LocalDate.now().isAfter(holyday);
     }
 
 }
