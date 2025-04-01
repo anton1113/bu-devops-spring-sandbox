3c06b0cb34c2124472ee3daa6215798835bedf42
Anton Rasshchektaiev <anton.rasshchektaiev@auto1.com>
2025-04-01 09:43:30 +0200

    update version to 13

--- src/main/java/com/bu/db/budevopsspringsandbox/controller/GreetingsController.java
+++ src/main/java/com/bu/db/budevopsspringsandbox/controller/GreetingsController.java
@@ -8,6 +8,6 @@ public class GreetingsController {
 
     @GetMapping("/hello")
     public String hello() {
-        return "Hello from DevOps basics! (Version 1.12), with webhook";
+        return "Hello from DevOps basics! (Version 1.13), with webhook";
     }
 }
